bc4b87ebf2ed9923173ea715dbb0e27747915b10
vish <mvishnurana@gmail.com>
2025-04-27 14:35:43 +0800

    Implemented a landing-page for the app

--- CheckMate.App/src/app/pages/langing-page/langing-page.component.ts
+++ CheckMate.App/src/app/pages/langing-page/langing-page.component.ts
@@ -1,13 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { catchError, EMPTY, first, tap } from 'rxjs';
-import { Router } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-langing-page',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatToolbarModule],
   providers: [HttpClient],
   templateUrl: './langing-page.component.html',
   styleUrl: './langing-page.component.scss'
@@ -15,41 +14,5 @@ import { Router } from '@angular/router';
 export class LangingPageComponent {
   constructor(
     readonly httpClient: HttpClient,
-    private router: Router,
   ) {}
-
-  login() {
-    window.location.href = 'https://localhost:7086/login';
-
-    const url = new URL(window.location.href);
-    const token = url.searchParams.get('token');
-
-    if (token) {
-      console.log('JWT from URL:', token);
-      localStorage.setItem('access_token', token);
-    } else {
-      window.location.href = 'https://localhost:7086/login';
-      console.log('here');
-    }
-  }
-
-  getWeatherData() {
-    this.httpClient.get('https://localhost:7086/WeatherForecast').pipe(
-      tap(x => {
-        this.router.navigate(['/signin-google'])
-        console.log(x);
-      }),
-      catchError((err) => {
-        console.log(err);
-
-        return EMPTY;
-      })
-    )
-    .subscribe();
-  }
-
-  signOut(): void {
-    localStorage.removeItem('access_token');
-    this.router.navigate(['/login']);
-  }
 }
